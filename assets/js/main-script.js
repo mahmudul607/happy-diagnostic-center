3d82a8118956f7c83bc7fc9fd412545d2303af6f
unknown <mahmudul.cse.eng@gmail.com>
2023-10-23 00:17:46 +0600

    contact page design

--- assets/js/main-script.js
+++ assets/js/main-script.js
@@ -178,6 +178,36 @@ window.onscroll = function(){
 }
 
 
+// google map
+
+// Initialize and add the map
+let map;
+
+async function initMap(map) {
+  // The location of Uluru
+  const position = { lat: 23.80714091906524, lng: 90.40730777463807 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "330cd246d4b8c715",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Dhaka",
+  });
+}
+
+initMap(map);
+
 
 //  circle ==========================================================================
 // let number = document.getElementById('number1');
